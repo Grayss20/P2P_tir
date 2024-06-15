1200dde03a8c1463d387f256ec71260ef8cef002
OlegBezverkhy <124727891+OlegBezverkhy@users.noreply.github.com>
2024-06-15 14:42:46 +0300

    add  const (target, icon)

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import pygame
+from random import randint
 
 pygame.init()
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGH = 600
+SCREEN_HEIGHT = 600
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGH))
-
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# Задаем иконку
 pygame.display.set_caption('Игра в тир')
-icon = pygame.image.load('')
+icon = pygame.image.load('images/shooter.jpg')
+pygame.display.set_icon(icon)
+# Задаем цель
+target_image = pygame.image.load('images/gangster.png')
+target_width = 80
+target_height = 100
+# Задаем фоновый рисунок
+color = (randint(0,255), randint)
 
+target_x = randint(0, SCREEN_WIDTH - target_width)
+target_y = randint(0, SCREEN_HEIGHT - target_height)
+color = (randint(0, 255), randint(0, 255), randint(0, 255))
 
 running = True
 while running:
