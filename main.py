39132f4d55d998ae6caf71957b08b8815ff58e9a
OlegBezverkhy <124727891+OlegBezverkhy@users.noreply.github.com>
2024-06-15 14:03:48 +0300

    add new const

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGH = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGH))
+
+pygame.display.set_caption('Игра в тир')
+icon = pygame.image.load('')
+
 
 running = True
 while running:
